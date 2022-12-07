a5d61e27f158e516323445cb36aec175491c0156
ADavidOliva <118214446+ADavidOliva@users.noreply.github.com>
2022-12-07 20:58:24 +0100

    Update main.java

--- ExamenEntorno02/src/main.java
+++ ExamenEntorno02/src/main.java
@@ -3,7 +3,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
-		System.out.println("hola me llamo");
+		System.out.println("hola me llamo david");
 
 	}
 
